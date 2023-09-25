d86b467b9199665a8af83d53f78b6020621367d5
maciejphp <kwiatkowski.maciej22@gmail.com>
2023-09-25 16:59:02 +0200

    added da files

---
+++ scr/sendemail.php
@@ -0,0 +1,53 @@
+<?php
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'kwiatkowski.maciej22@gmail.com';                     //SMTP username
+    $mail->Password   = file_get_contents('../password.txt');                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('kwiatkowski.maciej22@gmail.com', $_POST["name"]);
+    $mail->addAddress('40202811@roctilburg.nl');     //Add a recipient
+
+    //Content
+    // $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $_POST["email"];
+    $mail->Body    = $_POST["message"];
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br><br><a href="index.html">terug</a>
+</body>
+</html>
